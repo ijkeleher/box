cab20b9e767531d9bc58709aeafb011495905e47
Isaac Keleher <isaac.keleher@gmail.com>
2019-08-15 14:44:58 +1000

    Adding node modules file to .gitignore

---
+++ backend/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Uploading File\n")
+
+	//1. parse input, type multipart/form-data
+
+	r.ParseMultipartForm(10 << 20)
+
+	//2. retrieve file from posted form-data
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error retrieving file from form-data")
+		fmt.Println(err)
+		return
+	}
+	//defer the closing of the file until the end of the function
+	defer file.Close()
+
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("Uploaded Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	//3. write temporary file on server
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//defer the closing of the temp file until the end of the function
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	tempFile.Write(fileBytes)
+
+	//return sucess or failure
+
+	fmt.Fprintf(w, "Sucessfully uplaoded file\n")
+}
+
+func setUpRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Welcome to GoShare, a file sharing app written in Go")
+	setUpRoutes()
+}
